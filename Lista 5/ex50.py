403bb69807872538328a4df7db5c9fe262e80c55
andrecreppe <andrecrepper@gmail.com>
2020-06-07 17:51:05 -0300

    Update ex50.py

--- Lista 5/ex50.py
+++ Lista 5/ex50.py
@@ -4,33 +4,33 @@ class Product(object):
     qtd:int = 0
 
     #SETs
-    def setId(id):
+    def setId(self, id):
         self.id = id
-    def setCost(cost):
+    def setCost(self, cost):
         self.cost = cost
-    def setQtd(qtd):
+    def setQtd(self, qtd):
         self.qtd = qtd
 
     #GETs
-    def getId():
+    def getId(self):
         return self.id
-    def getCost():
+    def getCost(self):
         return self.cost
-    def getQtd():
+    def getQtd(self):
         return self.qtd
 
     #Methods
-    def getTotal():
+    def getTotal(self):
         return self.cost * self.qtd
     
-    def addQtd(add):
+    def addQtd(self, add):
         self.qtd += add
 
     def __str__(self):
         return '#{}:    {}  {:.2f} {:.2f}\n'.format(self.id, self.qtd, self.cost, self.getTotal())
 
-    #def str(self):
-        #return '#{}:    {}  {:.2f} {:.2f}\n'.format(self.id, self.qtd, self.cost, self.getTotal())
+    def toString(self):
+        return '#{}:    {}  {:.2f} {:.2f}\n'.format(self.id, self.qtd, self.cost, self.getTotal())
 
 class Recipt:
     items = []
@@ -38,7 +38,53 @@ class Recipt:
     def finalValue(self):
         tot = 0.0
 
+        for p in self.items:
+            tot += p.getTotal()
+
+        return tot
+
+    def addProduct(self, prod:Product):
+        self.items.append(prod)
+
+    def addQtd(self, id, qtd):
+        for p in self.items:
+            if p.getId() == id:
+                p.addQtd(qtd)
+                break
+
+    def __str__(self):
+        self.items = sorted(self.items, key = Product.getId)
+
+        output = '#COD QTD VL_UN R$\n'
+
+        for p in self.items:
+            if p.getQtd() > 0:
+                output += p.toString()
+
+        output += 'Total: R$ {:.2f}'.format(self.finalValue())
+
+        return output
+
 #Main
+rp = Recipt()
+
 qtd = int(input())
 
 for i in range(qtd):
+    prod = Product()
+
+    id, price = map(float, input().split(" "))
+
+    prod.setId(int(id))
+    prod.setCost(price)
+
+    rp.addProduct(prod)
+
+qtd = int(input())
+
+for i in range(qtd):
+    id, qtd = map(int, input().split(" "))
+
+    rp.addQtd(id, qtd)
+
+print(rp)
