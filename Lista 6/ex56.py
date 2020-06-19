ff08282a9731bfe1110a10a4bc1d5c2d41b112f0
andrecreppe <andre.creppe@uol.com.br>
2020-06-19 10:36:42 -0300

    Lista 6 - Start

---
+++ Lista 6/ex56.py
@@ -0,0 +1,36 @@
+import pickle as pk
+
+class Funcionario (object):
+    def __init__(self, nome, idade, sexo, salario):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.salario = salario
+
+#Main
+f = open("b:/Programming/IPE/Lista 6/dados-funcionario.bin", "rb")
+
+total = pk.load(f)
+
+totM = 0
+totW = 0
+sumSalM = 0
+sumSalW = 0
+
+for i in range(total):
+    line = pk.load(f)
+
+    if(line.sexo == 'M'):
+        tM += 1
+        sumSalM += line.salario
+    else:
+        tW += 1
+        sumSalW += line.salario
+
+f.close()
+
+avgSal = (sumSalM + sumSalW) / total
+avgSalM = sumSalM / totM
+avgSalW = sumSalW / totW
+
+print(total, totM, totW, f'{avgSal:.2f}', f'{avgSalM:.2f}', f'{avgSalW:.2f}')
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # IPE
-Github para salvar os exercícios feitos na disciplina de SCC xxx - Introdução a programação para as engenharias. USP de São Carlos
+Exercícios em Python feitos para a disciplina de SSC0304 - "Introdução à Programação para Engenharias" (2020), dada pelo ICMC da USP de São Carlos.
